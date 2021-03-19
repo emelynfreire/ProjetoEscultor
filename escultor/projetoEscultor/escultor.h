6e29bdf0c8872811e2d713eb37294041fb039768
Emelyn Freire <emelynfreire@outlook.com>
2021-03-19 01:11:06 -0300

    Update escultor.h
    correção palavra linha 28

--- escultor/projetoEscultor/escultor.h
+++ escultor/projetoEscultor/escultor.h
@@ -23,7 +23,7 @@ public:
     void setColor(float _r, float _g, float _b, float _a); //mudar a cor que será aplicada ao voxel
     void putVoxel(int x, int y, int z); //coloca um voxel (isOn = true)
     void cutVoxel(int x, int y, int z); //retira um voxel (isOn = false)
-    void putBox(int x0, int x1, int y0, int y1, int z0, int z1); //coloca uma raixa de voxels
+    void putBox(int x0, int x1, int y0, int y1, int z0, int z1); //coloca uma caixa de voxels
     void cutBox(int x0, int x1, int y0, int y1, int z0, int z1); //retira uma caixa de voxels
     void putSphere(int xcenter, int ycenter, int zcenter, int radius); //coloca uma esfera de voxels
     void cutSphere(int xcenter, int ycenter, int zcenter, int radius); //retira uma esfera de voxels
